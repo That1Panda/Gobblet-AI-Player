abae69cb805506475bb2ffaadadf81c8378317da
That1Panda <pandahd75@gmail.com>
2023-12-16 15:19:10 +0200

    [Development] Adding entities and their sizes
    - Added entity info in __init__function in gameBoard.py
    - Added draw_main_grid function in gameBoard.py
    - Added draw_extra_grid function in gameBoard.py
    - Added draw_circles function in gameBoard.py
    - Changed draw_board function in gameBoard.py

--- gameBoard.py
+++ gameBoard.py
@@ -36,14 +36,19 @@ class GameBoard:
 
     def __init__(self, grid_size: int):
         """
-        Initializes the game board with the given grid size.
+        Initializes the game board with the given grid size and player entities.
 
         Args:
             grid_size (int): the size of the game board
+            player_entities (List[List[int]]): list of player entities with initial circle sizes
         """
         self.GRID_SIZE = grid_size
         self.SQUARE_SIZE = 400 // grid_size
-        self.EXTRA_SPACE = 2  # Number of extra grid blocks on each side
+        self.EXTRA_SPACE = 2
+        self.stacks=3
+        self.entities=4
+        self.entity_size=15
+        self.player_entities = [[4, 1, 2], [3, 4, 2]]
         self.board = [[' ' for _ in range(grid_size)] for _ in range(grid_size)]
         self.WIDTH = (grid_size + 2 * self.EXTRA_SPACE) * self.SQUARE_SIZE
         self.HEIGHT = grid_size * self.SQUARE_SIZE
@@ -52,11 +57,10 @@ class GameBoard:
         self.WHITE = (255, 255, 255)
         self.BLACK = (0, 0, 0)
 
-    def draw_board(self):
+    def draw_main_grid(self):
         """
-        Draws the game board on the screen.
+        Draw the main 4x4 grid on the screen.
         """
-        self.screen.fill(self.BLACK)
         for row in range(self.GRID_SIZE):
             for col in range(self.EXTRA_SPACE, self.GRID_SIZE + self.EXTRA_SPACE):
                 pygame.draw.rect(self.screen, self.BLACK,
@@ -70,18 +74,50 @@ class GameBoard:
                                        ((col - self.EXTRA_SPACE) * self.SQUARE_SIZE + self.SQUARE_SIZE // 2,
                                         row * self.SQUARE_SIZE + self.SQUARE_SIZE // 2),
                                        self.SQUARE_SIZE // 3)
-        for row in range(3):
-            for col in range(1):
-                # divide the board into 3 parts where each part is a square make it a 3x1 big column on the left side
-                pygame.draw.rect(self.screen, self.BLACK,
-                                 (col * self.SQUARE_SIZE*2, row * int((self.SQUARE_SIZE*4)/3), self.SQUARE_SIZE*2, int((self.SQUARE_SIZE*4)/3)), 0)
-                pygame.draw.rect(self.screen, self.WHITE,
-                                    (col * self.SQUARE_SIZE*2, row * int((self.SQUARE_SIZE*4)/3), self.SQUARE_SIZE*2, int((self.SQUARE_SIZE*4)/3)), 1)
-                pygame.draw.rect(self.screen, self.BLACK,
-                                 (col * self.SQUARE_SIZE*2 + self.SQUARE_SIZE*6, row * int((self.SQUARE_SIZE*4)/3), self.SQUARE_SIZE*2, int((self.SQUARE_SIZE*4)/3)), 0)
-                pygame.draw.rect(self.screen, self.WHITE,
-                                    (col * self.SQUARE_SIZE*2 + self.SQUARE_SIZE*6, row * int((self.SQUARE_SIZE*4)/3), self.SQUARE_SIZE*2, int((self.SQUARE_SIZE*4)/3)), 1)
 
+    def draw_extra_columns(self):
+        """
+        Draw the extra columns on the left and right sides.
+        """
+        for row in range(self.stacks):
+            
+                x_left =0
+                y_left = row * int((self.SQUARE_SIZE * 4) / self.stacks)
+                pygame.draw.rect(self.screen, self.BLACK, (x_left, y_left, self.SQUARE_SIZE * 2, int((self.SQUARE_SIZE * 4) / self.stacks)), 0)
+                pygame.draw.rect(self.screen, self.WHITE, (x_left, y_left, self.SQUARE_SIZE * 2, int((self.SQUARE_SIZE * 4) / self.stacks)), 1)
+
+                x_right = self.SQUARE_SIZE * 6
+                y_right = row * int((self.SQUARE_SIZE * 4) / self.stacks)
+                pygame.draw.rect(self.screen, self.BLACK, (x_right, y_right, self.SQUARE_SIZE * 2, int((self.SQUARE_SIZE * 4) / self.stacks)), 0)
+                pygame.draw.rect(self.screen, self.WHITE, (x_right, y_right, self.SQUARE_SIZE * 2, int((self.SQUARE_SIZE * 4) / self.stacks)), 1)
+
+    def draw_circles(self):
+        """
+        Draw circles on the screen based on player entities.
+        """
+        for row in range(self.stacks):
+            
+                # Draw left side circles and make them dependent on player entities number (the bigger number the bigger circle)
+                x_left = self.SQUARE_SIZE 
+                y_left = row * int((self.SQUARE_SIZE * 4) / self.stacks) + int((self.SQUARE_SIZE * 4) / self.stacks)/2
+                pygame.draw.circle(self.screen, (255, 0, 0), (x_left, y_left), self.player_entities[0][row]*self.entity_size)
+
+                # Draw right side circles and make them dependent on player entities number (the bigger number the bigger circle)
+                x_right = self.SQUARE_SIZE * 6 + self.SQUARE_SIZE
+                y_right = row * int((self.SQUARE_SIZE * 4) / self.stacks)  + int((self.SQUARE_SIZE * 4) / self.stacks)/2
+                pygame.draw.circle(self.screen, (0, 0, 255), (x_right, y_right), self.player_entities[1][row]*self.entity_size)
+
+
+
+
+    def draw_board(self):
+        """
+        Draws the game board on the screen.
+        """
+        self.screen.fill(self.BLACK)
+        self.draw_main_grid()
+        self.draw_extra_columns()
+        self.draw_circles()
         pygame.display.flip()
 
     def check_win(self, player: str) -> bool:
